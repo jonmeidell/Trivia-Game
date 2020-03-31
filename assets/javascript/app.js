98201d24607adbee4bc117dac29ec80c6e4b0d22
jonmeidell <jonmeidell@gmail.com>
2020-03-30 21:02:44 -0600

    minor changes

---
+++ assets/javascript/app.js
@@ -0,0 +1,56 @@
+var questions = [
+    {
+        question : "Which person leads the X-Men?",
+        imgSrc : "",
+        choiceA : "",
+        choiceB : "",
+        choiceC : "",
+        choiceD : "",
+        correct : ""
+    },{
+        question : "",
+        imgSrc : "",
+        choiceA : "",
+        choiceB : "",
+        choiceC : "",
+        choiceD : "",
+        correct : ""
+    },{
+        question : "",
+        imgSrc : "",
+        choiceA : "",
+        choiceB : "",
+        choiceC : "",
+        choiceD : "",
+        correct : ""
+    },{
+        question : "",
+        imgSrc : "",
+        choiceA : "",
+        choiceB : "",
+        choiceC : "",
+        choiceD : "",
+        correct : ""
+    },{
+        question : "",
+        imgSrc : "",
+        choiceA : "",
+        choiceB : "",
+        choiceC : "",
+        choiceD : "",
+        correct : ""
+    }
+];
+
+var startGame = document.getElementById("startGame");
+var quizBody = document.getElementById("quizBody");
+var questions = document.getElementById("questions");
+var questionImages = document.getElementById("questionImages");
+var choiceA = document.getElementById("choiceA");
+var choiceB = document.getElementById("choiceB");
+var choiceC = document.getElementById("choiceC");
+var choiceD = document.getElementById("choiceD");
+var counter = document.getElementById("counter");
+var timeGauge = document.getElementById("timeGauge");
+var score = document.getElementById("score");
+var scoreContainer = document.getElementById("scoreContainer");
